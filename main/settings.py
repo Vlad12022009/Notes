4279d7d1c4acefc91f6ded097667ab0e643853d1
Vlad12022009 <vladikus1202@gmail.com>
2024-10-06 18:32:46 +0300

    add hosts

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2e7k^5_6w&zk3&ocjm21yf1$t(a34_s#=^#sg(tdxsosh(io%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Vlad12022009.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
